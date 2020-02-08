b6a4c2f8f25f2cdd71e32dced1de4b74b4ac4a27
Rick Belson <belsonrj@aol.com>
2020-02-08 23:32:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,30 @@
 class Song 
+  @@count = 0
   
-  @@count = 0 
+  def self.count 
+    @@count
+  end 
+  
+  # Control for duplicates when you code your 
+  # .genres class method, not when you add 
+  # genres to the original @@genres array.
   @@genres = []
   
-  def initialize
-    @@song_count += 1 
+# this should be deleted at some point
+# def self.total_genre_entries
+#   @@genres
+# end
+  
+  def self.genres
+    @@genres.uniq
   end
   
-  def self.genres 
-    @@genres.uniq 
-  end
+  # this method is the one I found on stackexchange...
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
   
+  # This is how the method is done on learn.co:
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
@@ -23,12 +37,23 @@ class Song
     genre_count
   end
   
-  def self.count 
-    @@song_count
-  end 
-
-  self.genre_count
-    @@genre_count = {}
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
 end 
-
